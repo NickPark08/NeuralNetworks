557beb8723a5151270aade00b1fcf065d5efd0d2
Nicholas Park <vinscully@outlook.com>
2024-10-11 17:35:32 -0700

    Add project files.

---
+++ NeuralNetworks/HillClimber.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworks
+{
+    internal class HillClimber
+    {
+        string Target;
+        string randomString;
+        Random random;
+        public HillClimber(string target, Random ran)
+        {
+            Target = target;
+            random = ran;
+            for(int i = 0; i < Target.Length; i++)
+            {
+                randomString += random.Next(32, 127);
+            }
+        }
+
+        public string Run()
+        {
+            while(GetError() != 0)
+            {
+                Mutate();
+            }
+
+            return randomString;
+        }
+
+        private double GetError()
+        {
+            int error = 0;
+            for(int i = 0; i < Target.Length; i++)
+            {
+                error += Math.Abs(Target[i] - randomString[i]);
+            }
+            return error / Target.Length;
+        }
+
+        private void Mutate()
+        {
+            StringBuilder sb = new StringBuilder(randomString, Target.Length);
+            int ranIndex = random.Next(Target.Length);
+            if (randomString[ranIndex] == 32)
+            {
+                sb[random.Next(Target.Length)]++;
+            }
+            else if (randomString[ranIndex] == 126)
+            {
+                sb[random.Next(Target.Length)]--;
+            }
+            else
+            {
+                if(random.Next(2) == 1)
+                {
+                    sb[random.Next(Target.Length)]++;
+                }
+                else
+                {
+                    sb[random.Next(Target.Length)]--;
+                }
+            }
+
+        }
+    }
+}
---
+++ NeuralNetworks/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace NeuralNetworks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            HillClimber climber = new HillClimber("test", random);
+
+            Console.WriteLine(climber.Run());
+        }
+    }
+}
