9ae6e47ac65345f9897fe892443d205130b39ce7
Nicholas.Park <Nicholas.Park@GMR-118-1-3>
2025-02-19 17:32:34 -0800

    fixed comparer ref error, implement pruning

--- NeuralNetworks/MiniMax.cs
+++ NeuralNetworks/MiniMax.cs
@@ -15,8 +15,6 @@ namespace NeuralNetworks
     }
     public partial class MiniMax<T> where T : IGameState<T>
     {
-        IComparer<Node> comparer;
-
         class MaxComparer : IComparer<Node>
         {
             private MaxComparer() { }
@@ -34,6 +32,7 @@ namespace NeuralNetworks
             public int Compare(Node x, Node y) => x.Value.CompareTo(y.Value);
         }
 
+        //IComparer<Node> comparer;
         public T Opossum;
         public Node Monkey;
         public Node Gorilla;
@@ -51,56 +50,89 @@ namespace NeuralNetworks
                 Value = state.Value;
                 Children = new Node[state.GetChildren().Length];
             }
+
+            public override string ToString()
+            {
+                return Value.ToString() + "\n" + State.ToString(); 
+            }
         }
 
         public void BuildTree(Node node, bool isMax, int a = int.MinValue, int b = int.MaxValue)
         {
-            comparer = isMax ? MaxComparer.Instance : MinComparer.Instance;
+            IComparer<Node> comparer = isMax ? MaxComparer.Instance : MinComparer.Instance;
             var childrenStates = node.State.GetChildren();
+
+            if (comparer == MaxComparer.Instance)
+            {
+                ;
+            }
+
             node.Children = new Node[childrenStates.Length];
 
             for (int i = 0; i < childrenStates.Length; i++)
             {
                 node.Children[i] = new Node(childrenStates[i]);
                 BuildTree(node.Children[i], !isMax, a, b);
-                for (int j = i; j > 0; j--)
+                //for (int j = i; j > 0; j--)
+                //{
+                //    if (comparer.Compare(node.Children[j], node.Children[j - 1]) < 0)
+                //    {
+                //        (node.Children[j], node.Children[j - 1]) = (node.Children[j - 1], node.Children[j]);
+                //    }
+                //}
+
+                ////pruning
+                //if (isMax)
+                //{
+                //    //a = node.Children[0].Value;
+                //    a = Math.Max(a, node.Children[0].Value);
+                //    if (b <= a) break;
+                //}
+                //else
+                //{
+                //    b = Math.Min(b, node.Children[0].Value);
+                //    if (a <= b) break;
+                //}
+            }
+
+            if (node.Children.Any(m => m.Value == 0))
+            {
+                ;
+            }
+            if (comparer == MinComparer.Instance && isMax)
+            {
+                ;
+            }
+            if(node == Monkey)
+            {
+                ;
+            }
+
+            if (node.Children.Length > 0 && !node.State.IsTerminal)
+            {
+                for (int j = node.Children.Length - 1; j > 0; j--)
                 {
                     if (comparer.Compare(node.Children[j], node.Children[j - 1]) < 0)
                     {
                         (node.Children[j], node.Children[j - 1]) = (node.Children[j - 1], node.Children[j]);
                     }
                 }
-
-                //pruning
-                if (isMax && node.Children[0].Value > a)
-                {
-                    //a = node.Children[0].Value;
-                    a = Math.Max(a, node.Children[0].Value);
-                    if (b <= a) break;
-                }
-                else if (!isMax && node.Children[0].Value < b)
-                {
-                    b = Math.Min(b, node.Children[0].Value);
-                    if (b <= a) break;
-                }
-            }
-
-            if (node.Children.Length > 0 && !node.State.IsTerminal)
-            {
                 //node.Children = node.Children.OrderBy(n => n.Value).ToArray();
-                node.Value = isMax ? node.Children[^1].Value : node.Children[0].Value;
+                //node.Value = isMax ? node.Children[^1].Value : node.Children[0].Value;
+                node.Value = node.Children[0].Value;
             }
         }
 
         public Node FindBestMove(Node node) => node.Children.First();
-            //if (isMax)
-            //{
-            //    return node.Children[^1];
-            //}
-            //else
-            //{
-            //    return node.Children[0];
-            //}
-        
+        //{
+        //    if (isMax)
+        //    {
+        //        return node.Children[^1];
+        //    }
+        //    else
+        //    {
+        //        return node.Children[0];
+        //    }
+        //}
     }
 }
