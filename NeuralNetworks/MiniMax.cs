7b4f2c119d6a49aa031792b7ae9b2635eebe2f80
Nicholas <Nicholas.Park@scsstudent.org>
2025-02-21 15:55:01 -0800

    wip: see if pruning is happening

--- NeuralNetworks/MiniMax.cs
+++ NeuralNetworks/MiniMax.cs
@@ -21,7 +21,7 @@ namespace NeuralNetworks
             public static MaxComparer Instance { get; } = new();
 
             public int Compare(Node x, Node y) => -x.Value.CompareTo(y.Value);
-            
+
         }
         class MinComparer : IComparer<Node>
         {
@@ -53,19 +53,16 @@ namespace NeuralNetworks
 
             public override string ToString()
             {
-                return Value.ToString() + "\n" + State.ToString(); 
+                return Value.ToString() + "\n" + State.ToString();
             }
         }
 
         public void BuildTree(Node node, bool isMax, int a = int.MinValue, int b = int.MaxValue)
         {
+            if (node.State.IsTerminal) return;
+
             IComparer<Node> comparer = isMax ? MaxComparer.Instance : MinComparer.Instance;
             var childrenStates = node.State.GetChildren();
-
-            if (comparer == MaxComparer.Instance)
-            {
-                ;
-            }
 
             node.Children = new Node[childrenStates.Length];
 
@@ -73,66 +70,64 @@ namespace NeuralNetworks
             {
                 node.Children[i] = new Node(childrenStates[i]);
                 BuildTree(node.Children[i], !isMax, a, b);
-                //for (int j = i; j > 0; j--)
-                //{
-                //    if (comparer.Compare(node.Children[j], node.Children[j - 1]) < 0)
-                //    {
-                //        (node.Children[j], node.Children[j - 1]) = (node.Children[j - 1], node.Children[j]);
-                //    }
-                //}
 
-                ////pruning
+                if (node.Children.Length > 0 && !node.State.IsTerminal)
+                {
+                    for (int j = node.Children.Length - 1; j > 0; j--)
+                    {
+                        if ((node.Children[j] != null && node.Children[j - 1] != null) && (comparer.Compare(node.Children[j], node.Children[j - 1]) < 0))
+                        {
+                            (node.Children[j], node.Children[j - 1]) = (node.Children[j - 1], node.Children[j]);
+                        }
+                    }
+                }
+                //node.Children.Order();
+
+
+                if (isMax)
+                {
+                    a = Math.Max(a, node.Children[0].Value);
+                    if (b <= a)
+                        break;
+                }
+                else
+                {
+                    b = Math.Min(b, node.Children[0].Value);
+                    if (b <= a)
+                        break;
+                }
                 //if (isMax)
                 //{
-                //    //a = node.Children[0].Value;
-                //    a = Math.Max(a, node.Children[0].Value);
-                //    if (b <= a) break;
+                //    node.Value = int.MinValue;
+                //    foreach (var child in node.Children)
+                //    {
+                //        node.Value = Math.Max(node.Value, child.Value);
+                //        a = Math.Max(a, node.Value);
+                //        if (b <= a)
+                //            break; // Beta cutoff
+                //    }
                 //}
                 //else
                 //{
-                //    b = Math.Min(b, node.Children[0].Value);
-                //    if (a <= b) break;
+                //    node.Value = int.MaxValue;
+                //    foreach (var child in node.Children)
+                //    {
+                //        node.Value = Math.Min(node.Value, child.Value);
+                //        b = Math.Min(b, node.Value);
+                //        if (b <= a)
+                //            break; // Alpha cutoff
+                //    }
                 //}
-            }
-
-            if (node.Children.Any(m => m.Value == 0))
-            {
-                ;
-            }
-            if (comparer == MinComparer.Instance && isMax)
-            {
-                ;
-            }
-            if(node == Monkey)
-            {
-                ;
             }
 
             if (node.Children.Length > 0 && !node.State.IsTerminal)
             {
-                for (int j = node.Children.Length - 1; j > 0; j--)
-                {
-                    if (comparer.Compare(node.Children[j], node.Children[j - 1]) < 0)
-                    {
-                        (node.Children[j], node.Children[j - 1]) = (node.Children[j - 1], node.Children[j]);
-                    }
-                }
-                //node.Children = node.Children.OrderBy(n => n.Value).ToArray();
-                //node.Value = isMax ? node.Children[^1].Value : node.Children[0].Value;
+
                 node.Value = node.Children[0].Value;
             }
         }
 
         public Node FindBestMove(Node node) => node.Children.First();
-        //{
-        //    if (isMax)
-        //    {
-        //        return node.Children[^1];
-        //    }
-        //    else
-        //    {
-        //        return node.Children[0];
-        //    }
-        //}
+
     }
 }
