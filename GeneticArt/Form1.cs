60a34636ffe7ca62ac27e3f432256ce1b6e06005
Nicholas Park <vinscully@outlook.com>
2025-05-19 17:31:10 -0700

    wip: start genetic art

---
+++ GeneticArt/Form1.cs
@@ -0,0 +1,10 @@
+namespace GeneticArt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GeneticArt/Triangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticArt
+{
+    class Triangle
+    {
+        Color color;
+        PointF points[];
+        public Triangle(PointF point0, PointF point1, PointF point2, Color color)
+        {
+
+        }
+
+        public void DrawTriangle(Graphics gfx, float xCoefficent, float yCoefficent)
+        {
+
+        }
+
+        public void Mutate(Random random)
+        {
+
+        }
+
+        public Triangle Copy()
+        {
+            return this;
+        }
+
+        public static Triangle RandomTriangle(Random random)
+        {
+
+        }
+    }
+}
